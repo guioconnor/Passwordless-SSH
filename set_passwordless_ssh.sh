67a623d85f08a2847272aa6d7d2dcbf1d9901ca2
Guilherme Zühlke O'Connor <guioconnor@gmail.com>
2010-01-14 16:16:27 +0000

    first commit

---
+++ set_passwordless_ssh.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+filename="id_rsa"
+path="$HOME/.ssh"
+
+if [ $1 -a $2 ]
+then
+    hostname=$1
+    username=$2
+else
+    # Read the host and username to store public key (the host/username accepting passwordless ssh from this computer)
+    echo "What host you want to grant passwordless SSH from this computer?"
+    read hostname
+    echo "What is your username on $hostname? ($USER?)"
+    read username
+
+    if [ ! $username ]
+    then
+        username="$USER"
+    fi
+fi
+
+
+# Generate rsa files
+if [ -f $path/$filename ]
+then
+    echo "RSA key exists on $path/$filename, using existing file"
+else
+    ssh-keygen -t rsa -f "$path/$filename"
+    echo RSA key pair generated
+fi
+
+echo "We need to log into $hostname as $username to set up your public key (hopefully last time you'll use password from this computer)" 
+cat "$path/$filename.pub" | ssh "$hostname" -l "$username" '[ -d .ssh ] || mkdir .ssh; cat >> .ssh/authorized_keys; chmod 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys'
+status=$?
+
+if [ $status -eq 0 ]
+then
+    echo "Set up complete, try to ssh to $host now"
+    exit 0
+else
+    echo "an error has occured"
+    exit 255
+fi
